f534e1aa1bffeffb9567f8bc1e3ea00e3f7a11c8
Tom Smith <tomgsmith99@gmail.com>
2023-04-09 19:53:34 -0400

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 var session = require('express-session')
 let mustacheExpress = require('mustache-express')
+var cors = require('cors')
 
 /*************************************************/
 
@@ -12,6 +13,10 @@ var dbconn = require('./dbconn.js')
 const app = express()
 
 app.use(express.json())
+
+app.use(express.static('public'))
+
+app.use(cors())
 
 app.engine('html', mustacheExpress())
 
